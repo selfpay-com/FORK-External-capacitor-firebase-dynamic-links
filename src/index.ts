0c0c5d3e3e7e7bb6f97d8d651b40eb084dcf08c3
narcis-ro <51541214+narcis-ro@users.noreply.github.com>
2021-10-20 18:29:09 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-import { registerPlugin } from '@capacitor/core';
-import type { CapacitorFirebaseDynamicLinksPlugin } from './definitions';
-
-const CapacitorFirebaseDynamicLinks = registerPlugin<CapacitorFirebaseDynamicLinksPlugin>('CapacitorFirebaseDynamicLinks', {
-  web: () => import('./web').then(m => new m.CapacitorFirebaseDynamicLinksWeb()),
-});
-
 export * from './definitions';
 export * from './web';
-export { CapacitorFirebaseDynamicLinks };
